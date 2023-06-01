d8a76e47f452d455916f08a5cd8524ab5ec44609
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-06-01 11:16:37 +0100

    added images

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
+import Image from "./troll-face-nathan-butcher.png"
+
 
 const Header = () => {
   return <div className="Header">
-    <img className="Head-Img" src="" />
+    <img className="Head-Img" src={Image} />
       <p>Meme Generator</p>
   </div>;
 };
