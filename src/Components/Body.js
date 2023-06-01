83e46d05ff05eab27fb7e1a3f3eff1ccc7165b38
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-06-01 11:10:33 +0100

    Updated the components

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Body.css";
+import Image from "./memeimg.png"
 
 const Hero = () => {
-  return <div></div>;
+  return <div className="Hero-Head">
+      <div className="Hero-Input">
+        <input type="text" />
+        <input type="text" />
+      </div>
+      <button>Get a new meme image</button>
+      <img className="Meme-Image" src={Image} />
+  </div>;
 };
 
 export default Hero;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div></div>;
+  return <div className="Header">
+    <img className="Head-Img" src="" />
+      <p>Meme Generator</p>
+  </div>;
 };
 
 export default Header;
