529a7e2efeb93a8a03ffde8ebbb0e3366e012e20
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-06-06 10:49:47 +0100

    Added api

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./Body.css";
 import Image from "./memeimg.png"
+import memesData from "./memesData.js"
 
 const Hero = () => {
+const getMemeImage = () => {
+const memesArray = memesData.data.memes
+console.log(memesArray)
+}
+
   return <div className="Hero-Head">
-      <div className="Hero-Input">
+<div className="Hero-Input">
         <input type="text" />
         <input type="text" />
       </div>
-      <button>Get a new meme image</button>
+      <button
+      onClick={getMemeImage}
+      >Get a new meme image</button>
       <img className="Meme-Image" src={Image} alt=""/>
   </div>;
 };
