c39b2ae2ab64a9df4111f4d8f4745e57ab3421dd
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-06-07 10:48:16 +0100

    Styled Components

---
+++ index.styled.js
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const MainBody = styled.body`
+  margin: 0;
+  padding: 0;
+`;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-import "./index.css";
+import { MainBody } from "./Components/Styled Components/Body.styled";
+import { Main } from "./Components/Styled Components/styled";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Body />
+    <MainBody>
+      <Header />
+      <Body />
+    </MainBody>
   </React.StrictMode>
 );
 
